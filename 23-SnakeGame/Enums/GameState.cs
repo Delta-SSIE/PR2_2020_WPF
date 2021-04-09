80cb46548f985dec48a5e2ee4b23a0398dd227aa
Jan Koupil <jan.koupil@delta-skola.cz>
2021-04-09 09:30:12 +0200

    GameController boilerplate code

---
+++ 23-SnakeGame/Enums/GameState.cs
@@ -0,0 +1,4 @@
+﻿namespace _23_SnakeGame
+{
+    enum GameState : byte { Stopped, Running, Won, Lost }
+}
---
+++ 23-SnakeGame/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_SnakeGame
+{
+    class GameController
+    {
+        private Snake _snake;
+        private GameMap _map;
+        private Direction _direction;
+        public GameState State {get; private set;}
+
+        public GameController()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Step()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
